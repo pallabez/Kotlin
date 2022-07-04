3a088c4f912f7f819e3dc33a319069291ed7f090
pallabez <pallabsonowal999@gmail.com>
2022-07-04 10:13:28 +0530

    add recycler view and adapter placeholder

---
+++ Affirmation/app/src/main/java/com/example/affirmation/adapter/ItemAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.affirmation.adapter
+
+import android.content.Context
+import com.example.affirmation.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
+
+}
